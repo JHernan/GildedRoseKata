e1d95710a9449869a05455690c4ddbb0a430ab7d
Jorge Hernán <jorge.hernan.esparza@gmail.com>
2015-03-14 11:18:26 +0100

    Refactor de capa semántica

--- src/Kata/GildedRose.php
+++ src/Kata/GildedRose.php
@@ -2,58 +2,138 @@
 namespace Kata;
 
 /**
- * Class Adder
+ * Class GildedRose
  * @package Kata
  */
 class GildedRose {
-    public static function updateQuality(
-        $items
-    ) {
+
+    const BRIE = "Aged Brie";
+    const PASS = "Backstage passes to a TAFKAL80ETC concert";
+    const SULFURAS = "Sulfuras, Hand of Ragnaros";
+    const DOUBLE_INCREMENT_THRESHOLD = 10;
+    const TRIPLE_INCREMENT_THRESHOLD = 5;
+    const SELLIN_GRANULARITY = 1;
+    const QUALITY_GRANULARITY = 1;
+    const MINIMUM_QUALITY = 0;
+    const MAXIMUM_QUALITY = 50;
+    const MINIMUN_SELLIN = 0;
+
+    public static function updateQuality($items) {
         for ($i = 0; $i < count($items); $i++) {
-            if (("Aged Brie" != $items[$i]->getName()) && ("Backstage passes to a TAFKAL80ETC concert" != $items[$i]->getName())) {
-                if ($items[$i]->getQuality() > 0) {
-                    if ("Sulfuras, Hand of Ragnaros" != $items[$i]->getName()) {
-                        $items[$i]->setQuality($items[$i]->getQuality() - 1);
-                    }
-                }
+            $currentItem = $items[$i];
+            $name = $currentItem->getName();
+            $notBrie = self::BRIE != $name;
+            $notPass = self::PASS != $name;
+            $isSulfuras = self::SULFURAS == $name;
+            $norBrieNeitherPass = ($notBrie) && ($notPass);
+            $isPass = self::PASS == $name;
+            $isBrie = self::BRIE == $name;
+
+            if ($norBrieNeitherPass) {
+                self::decreaseQuality($isSulfuras, $currentItem);
             } else {
-                if ($items[$i]->getQuality() < 50) {
-                    $items[$i]->setQuality($items[$i]->getQuality() + 1);
-                    if ("Backstage passes to a TAFKAL80ETC concert" == $items[$i]->getName()) {
-                        if ($items[$i]->getSellIn() < 11) {
-                            if ($items[$i]->getQuality() < 50) {
-                                $items[$i]->setQuality($items[$i]->getQuality() + 1);
-                            }
-                        }
-                        if ($items[$i]->getSellIn() < 6) {
-                            if ($items[$i]->getQuality() < 50) {
-                                $items[$i]->setQuality($items[$i]->getQuality() + 1);
-                            }
-                        }
+                self::increaseQuality($currentItem);
+                if ($isPass) {
+                    if (self::isInDoubleIncrement($currentItem)) {
+                        self::increaseQuality($currentItem);
+                    }
+                    if (self::isTripleIncrement($currentItem)) {
+                        self::increaseQuality($currentItem);
                     }
                 }
             }
-            if ("Sulfuras, Hand of Ragnaros" != $items[$i]->getName()) {
-                $items[$i]->setSellIn($items[$i]->getSellIn() - 1);
-            }
-            if ($items[$i]->getSellIn() < 0) {
-                if ("Aged Brie" != $items[$i]->getName()) {
-                    if ("Backstage passes to a TAFKAL80ETC concert" != $items[$i]->getName()) {
-                        if ($items[$i]->getQuality() > 0) {
-                            if ("Sulfuras, Hand of Ragnaros" != $items[$i]->getName()) {
-                                $items[$i]->setQuality($items[$i]->getQuality() - 1);
-                            }
-                        }
-                    } else {
-                        $items[$i]->setQuality($items[$i]->getQuality() - $items[$i]->getQuality());
-                    }
+            self::updateSellIn($isSulfuras, $currentItem);
+            if (self::isExpired($currentItem)) {
+                if ($norBrieNeitherPass) {
+                    self::decreaseQuality($isSulfuras, $currentItem);
                 } else {
-                    if ($items[$i]->getQuality() < 50) {
-                        $items[$i]->setQuality($items[$i]->getQuality() + 1);
-                    }
+                    self::resetQuality($currentItem);
+                }
+                if ($isBrie) {
+                    self::increaseQuality($currentItem);
                 }
             }
         }
+    }
+
+    /**
+     * @param $item
+     * @return bool
+     */
+    private function hasNotQuality($item){
+        return ($item->getQuality() <= self::MINIMUM_QUALITY);
+    }
+
+    /**
+     * @param $item
+     * @return bool
+     */
+    private function hasMaximumQuality($item){
+        return ($item->getQuality() >= self::MAXIMUM_QUALITY);
+    }
+
+    /**
+     * @param $isSulfuras
+     * @param $item
+     */
+    private function updateSellIn($isSulfuras, $item)
+    {
+        if ($isSulfuras) return;
+        $item->setSellIn($item->getSellIn() - self::SELLIN_GRANULARITY);
+    }
+
+    /**
+     * @param $isSulfuras
+     * @param $currentItem
+     */
+    private static function decreaseQuality($isSulfuras, $currentItem)
+    {
+        if ($isSulfuras && self::hasNotQuality($currentItem)) return;
+        $currentItem->setQuality($currentItem->getQuality() - self::QUALITY_GRANULARITY);
+    }
+
+    /**
+     * @param $currentItem
+     */
+    private static function increaseQuality($currentItem)
+    {
+        if (self::hasMaximumQuality($currentItem)) return;
+        $currentItem->setQuality($currentItem->getQuality() + self::QUALITY_GRANULARITY);
+    }
+
+    /**
+     * @param $currentItem
+     * @return bool
+     */
+    private static function isExpired($currentItem)
+    {
+        return $currentItem->getSellIn() < self::MINIMUN_SELLIN;
+    }
+
+    /**
+     * @param $currentItem
+     */
+    private static function resetQuality($currentItem)
+    {
+        $currentItem->setQuality(self::MINIMUM_QUALITY);
+    }
+
+    /**
+     * @param $currentItem
+     * @return bool
+     */
+    private static function isInDoubleIncrement($currentItem)
+    {
+        return $currentItem->getSellIn() <= self::DOUBLE_INCREMENT_THRESHOLD;
+    }
+
+    /**
+     * @param $currentItem
+     * @return bool
+     */
+    private static function isTripleIncrement($currentItem)
+    {
+        return $currentItem->getSellIn() <= self::TRIPLE_INCREMENT_THRESHOLD;
     }
 }
 ?>
